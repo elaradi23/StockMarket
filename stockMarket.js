9bc1e33476ee0250a8c6107f5e633c5d03851ffd
elaradi23 <ta.elaradi23@gmail.com>
2018-07-06 17:51:58 +0000

    Added comments

--- stockMarket.js
+++ stockMarket.js
@@ -1,4 +1,7 @@
 function stockMarket(array){
+  //Using divide and conquer, the elements of the array is chunked into pairs
+  //Every time the difference between the pair numbers is checked against a max value (which is inititated to 0)
+  //If the pair have a difference greater than the stored max, it's swapped
   var max = 0;
   for(var i = 0; i < array.length - 1; i++){
     for(var j = i + 1; j < array.length; j++){
@@ -8,13 +11,14 @@ function stockMarket(array){
     }
   }
 
+  //In case max difference was not changed, return 1
   if(max == 0){
     return -1;
   }
-
   return max;
 }
 
+//A method to calculate the difference between the bigger number and smaller number
 function diff(x, y){
   if (x > y){
     return x - y;
